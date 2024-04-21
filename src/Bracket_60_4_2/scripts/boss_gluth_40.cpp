10ed44e9ee344fd98c88872f4e3b2fd46ad884ee
Jelle Meeus <sogladev@gmail.com>
2024-04-21 03:39:30 +0200

    gluth adjust enrage with cast custom spell

--- src/Bracket_60_4_2/scripts/boss_gluth_40.cpp
+++ src/Bracket_60_4_2/scripts/boss_gluth_40.cpp
@@ -24,7 +24,7 @@
 enum Spells
 {
     SPELL_MORTAL_WOUND = 25646,
-    SPELL_ENRAGE = 90030,
+    SPELL_ENRAGE = 28371,
     SPELL_DECIMATE = 28374,
     SPELL_BERSERK = 26662,
     SPELL_INFECTED_WOUND = 29306,
@@ -179,16 +179,21 @@ public:
             if (me->HasUnitState(UNIT_STATE_CASTING))
                 return;
 
+
             switch (events.ExecuteEvent())
             {
             case EVENT_BERSERK:
                 me->CastSpell(me, SPELL_BERSERK, true);
                 break;
             case EVENT_ENRAGE:
+            {
                 Talk(EMOTE_ENRAGE);
-                me->CastSpell(me, SPELL_ENRAGE, true);
+                int32 bp1 = 99; // Enrage melee haste
+                int32 bp2 = 49; // Enrage damage percent
+                me->CastCustomSpell(me, SPELL_ENRAGE, &bp1, &bp2, 0, true);
                 events.RepeatEvent(22000);
                 break;
+            }
             case EVENT_MORTAL_WOUND:
                 me->CastSpell(me->GetVictim(), SPELL_MORTAL_WOUND, false);
                 events.RepeatEvent(10000);
