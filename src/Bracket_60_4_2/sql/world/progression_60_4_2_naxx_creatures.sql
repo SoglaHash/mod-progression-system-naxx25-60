a9d495ec15e6d762cd401f45cd250329d92c5a13
Jelle Meeus <sogladev@gmail.com>
2024-04-23 21:53:14 +0200

    eye stalk, fungal spore adjustment

--- src/Bracket_60_4_2/sql/world/progression_60_4_2_naxx_creatures.sql
+++ src/Bracket_60_4_2/sql/world/progression_60_4_2_naxx_creatures.sql
@@ -5659,3 +5659,19 @@ VALUES(29371, 'spell_heigan_eruption_40');
 DELETE FROM `spell_script_names` WHERE `spell_id` = 28158;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`)
 VALUES(28158, 'spell_grobbulus_poison_cloud_poison_40');
+
+-- Eye Stalk
+-- Reduce damage 2.5k to 750, movement speed reduction -50 to -20 (as unsigned int)
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 351090);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(351090, 0, 0, 0, 0, 0, 85, 0, 0, 0, 11000, 11000, 0, 0, 218, 29407, 0, 749, 4294967276, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Eye Stalk - In Combat - Custom Cast Mind Flay (Naxx40)'),
+(351090, 0, 1, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 26586, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Eye Stalk - On Respawn - Cast \'Birth\' (Naxx40)'),
+(351090, 0, 2, 0, 1, 0, 100, 0, 2000, 8000, 2000, 30000, 0, 0, 11, 28819, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Eye Stalk - Out of Combat - Cast \'Submerge Visual\' (Naxx40)'),
+(351090, 0, 3, 0, 9, 0, 100, 0, 0, 0, 2000, 2000, 36, 90, 24, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Eye Stalk - Within 36-90 Range - Evade (Naxx40)');
+
+-- Fungal Spore
+-- OnDeath Apply: Threat -100% (as unsigned int), Melee +50% crit, Casters +60% (up from 50%) crit
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 351066);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(351066, 0, 0, 0, 6, 0, 100, 512, 0, 0, 0, 0, 0, 0, 218, 29232, 2, 4294967196, 49, 59, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Spore - On Just Died - Custom Cast Fungal Creep (Naxx 40)');
+
