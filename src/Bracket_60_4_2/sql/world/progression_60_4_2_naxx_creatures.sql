e5aa3e4ae4ad5e1fcb086af12d537efc1c82f4ff
Jelle Meeus <sogladev@gmail.com>
2024-04-26 01:40:34 +0200

    update spells with anub

--- src/Bracket_60_4_2/sql/world/progression_60_4_2_naxx_creatures.sql
+++ src/Bracket_60_4_2/sql/world/progression_60_4_2_naxx_creatures.sql
@@ -5675,8 +5675,3 @@ DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 351066)
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (351066, 0, 0, 0, 6, 0, 100, 512, 0, 0, 0, 0, 0, 0, 218, 29232, 2, 4294967196, 49, 59, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Spore - On Just Died - Custom Cast Fungal Creep (Naxx 40)');
 
--- Anub'Arak
--- Locust Swarm: Reduce damage ~1500 to ~1000, increase radius 25yd to 30yd
-DELETE FROM `spell_script_names` WHERE `spell_id` = 28785;
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`)
-VALUES(28785, 'spell_anub_locust_swarm_40');
