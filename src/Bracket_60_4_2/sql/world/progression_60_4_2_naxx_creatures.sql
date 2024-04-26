2c0490e8ce45e5b7738edae1e6542c12fcfd7d60
Jelle Meeus <sogladev@gmail.com>
2024-04-26 02:01:52 +0200

    fix: Void Zone damage (Blaumeux/Nethersprite)
    https://github.com/azerothcore/azerothcore-wotlk/pull/18807

--- src/Bracket_60_4_2/sql/world/progression_60_4_2_naxx_creatures.sql
+++ src/Bracket_60_4_2/sql/world/progression_60_4_2_naxx_creatures.sql
@@ -5675,3 +5675,15 @@ DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 351066)
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (351066, 0, 0, 0, 6, 0, 100, 512, 0, 0, 0, 0, 0, 0, 218, 29232, 2, 4294967196, 49, 59, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Spore - On Just Died - Custom Cast Fungal Creep (Naxx 40)');
 
+-- Blaumeux
+-- Void Zone damage
+-- upstream handles void zone damage incorrectly. Azerothcore (#18807)
+DELETE FROM `spell_script_names` WHERE `spell_id` = 28865;
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(28865,'spell_four_horsemen_consumption');
+DELETE FROM `creature_template_spell` WHERE `CreatureID` = 16697;
+UPDATE `creature_template` SET `ScriptName` = '', `AIName` = 'SmartAI' WHERE `entry` = 16697;
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 16697);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(16697, 0, 0, 0, 60, 0, 100, 0, 0, 0, 2500, 2500, 0, 0, 11, 28865, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Void Zone - On Update - Cast \'Consumption\''),
+(16697, 0, 1, 0, 37, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Void Zone - On Initialize - Set Reactstate Passive');
